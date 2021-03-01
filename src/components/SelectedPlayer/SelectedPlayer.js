86d67efea0b2edddfe05f9103d2be8ed386a28a6
RASHIDUL KARIM <you@example.com>
2021-03-01 22:36:12 +0600

    almost done

--- src/components/SelectedPlayer/SelectedPlayer.js
+++ src/components/SelectedPlayer/SelectedPlayer.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const SelectedPlayer = (props) => {
     const selectedPlayer = props.selectedPlayer
-    const selectedPlayerName = selectedPlayer.reduce((player, selectedPlayer)=> player + selectedPlayer.name + ', ' ,[]) 
     const salary = selectedPlayer.reduce((salary, team) => salary+ team.salary ,0)
+    const selectedPlayerName = selectedPlayer.reduce((player, selectedPlayer)=> player + selectedPlayer.name + ' $'+ (selectedPlayer.salary) + ', ' ,[]) 
+    
     return (
         <div>
             <h5>No of Player Selected: {selectedPlayer.length}</h5>
-            <h5>Selected Player Name: {selectedPlayerName} </h5>
+            <h5>Selected Player: {selectedPlayerName} </h5>
             <h5>Budget: $ {salary}</h5>
         </div>
     );
